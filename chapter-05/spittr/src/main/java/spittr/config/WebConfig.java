56dc9fd3e49798a6934d18aba0a04b99e8c5ccea
YJ2CS <Blabber7846@outlook.com>
2025-01-07 22:06:27 +0800

    默认静态文件处理

--- chapter-05/spittr/src/main/java/spittr/config/WebConfig.java
+++ chapter-05/spittr/src/main/java/spittr/config/WebConfig.java
@@ -25,6 +25,14 @@ public class WebConfig extends WebMvcConfigurerAdapter {
 
     @Override
     public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        // 如果没有明确配置，DispatcherServlet 无法正确处理静态资源，会导致 404 错误。
+        // 通过将静态资源请求转发给默认 Servlet，可以避免额外的配置，同时确保静态资源能够正常访问。
+
+        // DispatcherServlet 专注于处理动态请求（如业务逻辑）。
+        //  默认 Servlet 专注于处理静态资源，职责分离更清晰
+
+        // 要求 DispatcherServlet 将对静态资源的请求转发到 Servlet 容器中默认的 Servlet 上，
+        // 而不是使用 DispatcherServlet 本身来处理此类请求
         configurer.enable();
     }
 
